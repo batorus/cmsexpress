9b4be3b7f6c4464d1bdb6f5be6b9bd7c98ab9e8d
batorus <batoru@yahoo.com>
2019-04-29 22:32:04 +0300

    crud wip

--- index.js
+++ index.js
@@ -2,38 +2,29 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Body Parser Middleware
+//for reading raw json and to be able to work with forms
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // app.get('/', (req, res)=>{
 
 //     res.sendFile(path.join(__dirname,'public','index.html'));
 // } )
 
-const members = [
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'john@gmail.com',
-      status: 'active'
-    },
-    {
-      id: 2,
-      name: 'Bob Williams',
-      email: 'bob@gmail.com',
-      status: 'inactive'
-    },
-    {
-      id: 3,
-      name: 'Shannon Jackson',
-      email: 'shannon@gmail.com',
-      status: 'active'
-    }
-  ];
 
-app.get("api/members/", (req,res)=>{
+  //get all members
+// app.get("/api/members/", (req,res)=>{
+//     res.json(members);
+// })
 
-})
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
